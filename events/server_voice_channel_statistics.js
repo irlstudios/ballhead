8d62a84db87a99956a4c68669d7306278aba39fa
roy-gymclassvr <roy.sterling@gymclassvr>
2025-05-27 16:50:49 -0500

    fixed data format for mixpanel

--- events/server_voice_channel_statistics.js
+++ events/server_voice_channel_statistics.js
@@ -3,15 +3,15 @@ const mixpanel = Mixpanel.init(process.env.MIXPANEL_TOKEN);
 
 module.exports = {
     name: 'voiceStateUpdate',
-    async execute(oldState, newState, client) {
+    async execute(oldState, newState) {
         if (
             oldState.channelId !== '960935833676955778' &&
             newState.channelId === '960935833676955778'
         ) {
             mixpanel.track('Stage Join', {
-                stage_id: newState.channelId,
-                user_id: newState.member.id,
-                date: Date.now(),
+                stage_id: String(newState.channelId),
+                user_id: String(newState.member.id),
+                date: new Date().toISOString(),
             });
         }
     },
